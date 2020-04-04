358630f70f0379fa0dca9f8900a5e5a48ee3e41e
undind <flyker777@yandex.ru>
2020-04-04 23:06:12 +0300

    added styles for siddebar

---
+++ src/components/Icons/index.js
@@ -0,0 +1,27 @@
+import { ReactComponent as Panels } from '../../assets/img/icons/panels.svg';
+import { ReactComponent as Child } from '../../assets/img/icons/child-panels.svg';
+import { ReactComponent as Domains } from '../../assets/img/icons/domains.svg';
+import { ReactComponent as Gateways } from '../../assets/img/icons/gateways.svg';
+import { ReactComponent as HostedEmail } from '../../assets/img/icons/hosted-email.svg';
+import { ReactComponent as Invoices } from '../../assets/img/icons/invoices.svg';
+import { ReactComponent as ParsingCredits } from '../../assets/img/icons/parsing-credits.svg';
+import { ReactComponent as Referrals } from '../../assets/img/icons/referrals-2.svg';
+import { ReactComponent as SettingsIcon } from '../../assets/img/icons/settings.svg';
+import { ReactComponent as Stores } from '../../assets/img/icons/stores.svg';
+import { ReactComponent as Support } from '../../assets/img/icons/support.svg';
+import { ReactComponent as WhatsNew } from '../../assets/img/icons/what-s-new.svg';
+
+export {
+    Panels,
+    Child,
+    Domains,
+    Gateways,
+    HostedEmail,
+    Invoices,
+    ParsingCredits,
+    Referrals,
+    SettingsIcon,
+    Stores,
+    Support,
+    WhatsNew,
+};
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,36 +1,105 @@
 import './_sidebar.scss';
 import React from 'react';
-import { NavItem, NavLink, Nav } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { NavItem, NavLink, Nav, Badge } from 'reactstrap';
+
+import Logo from '../../assets/img/sidebar/symbol.png';
+import {
+    Panels,
+    Child,
+    Domains,
+    Gateways,
+    HostedEmail,
+    Invoices,
+    ParsingCredits,
+    Referrals,
+    SettingsIcon,
+    Stores,
+    Support,
+    WhatsNew,
+} from '../Icons';
 
 const SideBar = (props) => (
     <div className='sidebar'>
         <div className='sidebar-header'>
-            <span color='info' style={{ color: '#fff' }}>
-                &times;
-            </span>
-            <h3>Perfect Panel</h3>
+            <div className='sidebar-header__logo'>
+                <img src={Logo} alt='Logo' />
+            </div>
+            <h3 className='sidebar-header__name'>Perfect Panel</h3>
         </div>
-        <div className='side-menu'>
-            <Nav vertical className='list-unstyled pb-3'>
+        <div className='sidebar-menu'>
+            <Nav vertical>
                 <NavItem>
-                    <NavLink tag={Link} to={'/about'}>
-                        About
+                    <NavLink active>
+                        <Panels />
+                        <span>Panels</span>
                     </NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink tag={Link} to={'/pages'}>
-                        Portfolio
+                    <NavLink>
+                        <Child />
+                        <span>Child panels</span>
                     </NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink tag={Link} to={'/faq'}>
-                        FAQ
+                    <NavLink>
+                        <Stores />
+                        <span>Stores</span>
                     </NavLink>
                 </NavItem>
                 <NavItem>
-                    <NavLink tag={Link} to={'/contact'}>
-                        Contact
+                    <NavLink>
+                        <Gateways />
+                        <span>Gateways</span>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <ParsingCredits />
+                        <span>Parsing credits</span>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <Domains />
+                        <span>Domains</span>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <HostedEmail />
+                        <span>Hosted email</span>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <Invoices />
+                        <span>Invoices</span>
+                        <Badge color="danger">3</Badge>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <Referrals />
+                        <span>Referrals</span>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <Support />
+                        <span>Support</span>
+                        <Badge color="warning">8</Badge>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <WhatsNew />
+                        <span>What's new</span>
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink>
+                        <SettingsIcon />
+                        <span>Settings</span>
                     </NavLink>
                 </NavItem>
             </Nav>
